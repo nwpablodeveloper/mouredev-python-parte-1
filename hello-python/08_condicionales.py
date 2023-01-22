91310c2294d25f4694d38801deb5c8eb55f3ba78
pabloDEVeiga <nwpablodeveloper@gmail.com>
2023-01-22 07:18:50 -0300

    Condicionales

---
+++ hello-python/08_condicionales.py
@@ -0,0 +1,36 @@
+# CONDICIONALES
+
+my_condition = True
+
+if my_condition:
+    print("Se ejecuta la condición del if")
+
+print("La ejecución continúa")
+
+my_condition = 1 * 1    
+
+if my_condition == 10:
+    print("Es 10")
+
+if my_condition >= 10:
+    print("Es mayor o igual a 10")
+
+if my_condition > 10 and my_condition < 20:
+    print("Es mayor 10 y menor a 20")
+elif my_condition == 1:
+    print("Es igual a 1")
+else:
+    print("Es menor o es mayor a 20")
+
+    print("Es menor o es mayor a 20")
+  
+
+
+print("La ejecución continúa")
+
+my_string = ""
+
+if not my_string:
+    print("La variabl esta vacia pero el False lo convierto a True")
+
+print("\n \n")

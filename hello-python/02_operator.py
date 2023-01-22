d224a047dee40ad47a24357437b96421442a2039
pabloDEVeiga <nwpablodeveloper@gmail.com>
2023-01-22 07:15:23 -0300

    Operadores

---
+++ hello-python/02_operator.py
@@ -0,0 +1,43 @@
+### OPERADORES ARITMETICOS
+
+print( "OPERADORES ARIMETICOS" )
+print( 3 + 4 )
+print( 3 - 4 )
+print( 3 * 4 )
+print( 3 / 4 )
+
+print( 10 % 4 ) # resto de la division 
+print( 10 // 3 ) # division entera ( redondea el resultado )
+print( 2 ** 4 ) # calcular potencia
+print( 2 ** 4 + 3 / 5 // 10  ) 
+
+print( "Hola" + "Python" )
+print( "Hola " * 5 )
+print( "Hola " * ( 2 ** 3) )
+
+
+### OPERADORES COMPARATIVOS
+
+print( 3 > 4 )
+print( 3 < 4 )
+print( 3 <= 4 )
+print( 3 >= 4 )
+print( 3 == 3 )
+print( 3 != 4 )
+
+print( "OPERADORES COMPARATIVOS" )
+print( "Hola" > "Python" )
+print( "Hola" < "Python" )
+print( "Hola" <= "Python" )
+print( "Hola" >= "Python" )
+print( "Hola" == "Hola" )
+print( "Hola" != "Python" )
+print( len("Hola") >= len("Python") )
+
+
+### OPERADORES LOGICOS
+
+print( "OPERADORES LÓGICOS" )
+print( 3 > 4 and "Hola" > "Python" )
+print( 3 > 4 or "Hola" > "Python" )
+print( not(3 > 4) )
